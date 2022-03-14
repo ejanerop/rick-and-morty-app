476e46e1e5c180c1e2a0a03e7ed289e43ea7a539
Eric Janero <ejanerop97@gmail.com>
2022-03-14 00:58:50 -0400

    Created main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './components/characters/characters.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
+import { HomeComponent } from './components/home/home.component';
+import { LocationsComponent } from './components/locations/locations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'episodes', component: EpisodesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
+import { CoverComponent } from './components/home/cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CharactersComponent,
+    LocationsComponent,
+    EpisodesComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
