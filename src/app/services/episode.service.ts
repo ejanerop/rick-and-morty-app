453aa1ab604494cf58193743ea18c3440ed6f8b2
Eric Janero <ejanerop97@gmail.com>
2022-03-14 22:15:52 -0400

    Created episodes service

---
+++ src/app/services/episode.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EpisodeService {
+  baseUrl: string = 'https://rickandmortyapi.com/api';
+  constructor(private http: HttpClient) {}
+
+  getEpisodes(term: string = '') {
+    const url = `${this.baseUrl}/episodes${
+      term.trim() != '' ? `?name=${term}` : ''
+    }`;
+
+    return this.http.get(url);
+  }
+}
