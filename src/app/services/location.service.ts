712f38e5259b7c2ad6db4519983dacd018506c39
Eric Janero <ejanerop97@gmail.com>
2022-03-16 00:42:45 -0400

    Created location service

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationService {
+  baseUrl: string = 'https://rickandmortyapi.com/api';
+
+  constructor(private http: HttpClient) {}
+
+  getLocations(term: string = '', page: number = 1) {
+    const url = `${this.baseUrl}/location?page=${page}${
+      term.trim() != '' ? `&name=${term}` : ''
+    }`;
+
+    return this.http.get(url);
+  }
+}
